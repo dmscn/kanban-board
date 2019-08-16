c65342021fcd2b0ff8c79468401c0b24ea61e808
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-08-16 13:13:25 -0300

    Added shaddows on cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,9 +15,13 @@ const Card: React.FC<CardProps> = styled.div<CardProps>`
   background: ${props =>
     props.color || cardColors[Math.floor(Math.random() * cardColors.length)]};
   cursor: grab;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
 
   &:active {
     cursor: grabbing;
+    box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+      0 10px 10px -5px rgba(0, 0, 0, 0.04);
   }
 `
 
