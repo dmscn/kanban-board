68bf5e2a0071b107412ffd6de24ab25eec16d34f
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-08-17 12:25:14 -0300

    Added task creation logic

--- src/containers/Board/Board.tsx
+++ src/containers/Board/Board.tsx
@@ -1,22 +1,42 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, FormEvent } from 'react'
+
+import { BoardContext, boardActions } from '../../ducks/board'
+import Task from '../../models/Task'
 
 import Box from '../../components/Box'
 import BoardColumn from '../BoardColumn'
-import { BoardContext } from '../../ducks/board'
+import TextInput from '../../components/TextInput'
 
 const Board: React.FC = () => {
-  const { state } = useContext(BoardContext)
+  const { state, dispatch } = useContext(BoardContext)
+  const inputRef = useRef(null)
+
+  const createTask = (event: FormEvent) => {
+    event.preventDefault()
+    // @ts-ignore
+    const task = new Task(inputRef.current.value)
+    // @ts-ignore
+    inputRef.current.value = ''
+    dispatch(boardActions.addTask(task, task.column))
+  }
 
   return (
-    <Box>
-      <Box column margin={10}>
-        <BoardColumn title="Todo" cards={state.todo} />
+    <Box column>
+      <Box justify="flex-end" padding={10}>
+        <form onSubmit={createTask}>
+          <TextInput ref={inputRef} placeholder="Nova task..." />
+        </form>
       </Box>
-      <Box column margin={10}>
-        <BoardColumn title="Doing" cards={state.doing} />
-      </Box>
-      <Box column margin={10}>
-        <BoardColumn title="Done" cards={state.done} />
+      <Box>
+        <Box column margin={10}>
+          <BoardColumn title="Todo" cards={state.todo} />
+        </Box>
+        <Box column margin={10}>
+          <BoardColumn title="Doing" cards={state.doing} />
+        </Box>
+        <Box column margin={10}>
+          <BoardColumn title="Done" cards={state.done} />
+        </Box>
       </Box>
     </Box>
   )
--- src/containers/BoardColumn/BoardColumn.tsx
+++ src/containers/BoardColumn/BoardColumn.tsx
@@ -1,25 +1,21 @@
-import React, { useState, useContext } from 'react'
-import { useDrop, DropTargetMonitor } from 'react-dnd'
-import DraggableCard from '../DraggableCard'
+import React, { useContext } from 'react'
+import { useDrop } from 'react-dnd'
+
 import { BoardContext, boardActions } from '../../ducks/board'
+import DraggableCard from '../DraggableCard'
+import Box from '../../components/Box'
+import Task from '../../models/Task'
 
 export interface BoardColumnProps {
   title: string
   cards: any[]
 }
 
-type CardType = {
-  type: string
-  column: string
-  position: number
-  element: any
-}
-
 const BoardColumn: React.FC<BoardColumnProps> = (props: BoardColumnProps) => {
   const { dispatch } = useContext(BoardContext)
   const [, drag] = useDrop({
     accept: 'Card',
-    drop: (card: CardType) => {
+    drop: (card: Task) => {
       if (card.column !== props.title) {
         dispatch(
           boardActions.moveTask(
@@ -33,20 +29,28 @@ const BoardColumn: React.FC<BoardColumnProps> = (props: BoardColumnProps) => {
   })
 
   return (
-    <div ref={drag}>
+    <Box
+      ref={drag}
+      column
+      bg="whitesmoke"
+      border="gainsboro"
+      borderRadius={3}
+      padding={20}
+      width={300}
+    >
       <span>{props.title}</span>
       {props.cards &&
         props.cards.map((card: any, index: number) => (
-          <DraggableCard
+          <Box
+            marginVertical={15}
             key={`${props.title}${card.id}${Math.random()}`}
-            column={props.title}
-            position={index}
-            card={card}
           >
-            {card.text}
-          </DraggableCard>
+            <DraggableCard column={props.title} position={index} card={card}>
+              {card.text}
+            </DraggableCard>
+          </Box>
         ))}
-    </div>
+    </Box>
   )
 }
 
